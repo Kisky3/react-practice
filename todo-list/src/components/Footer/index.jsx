597d964091dcfb1cfa5f34e7ec34676e51a50bbc
XiangYu <kisky3@126.com>
2022-01-05 20:04:26 +0900

    Footerの構造を作成

--- todo-list/src/components/Footer/index.jsx
+++ todo-list/src/components/Footer/index.jsx
@@ -4,7 +4,15 @@ export default class Footer extends Component {
     render() {
         return (
             <div>
-                Footer
+                <div className="todo-footer">
+                    <label>
+                        <input type="checkbox" />
+                    </label>
+                    <span> 
+                        <span>Finished Tasks 0</span> / All 2 Tasks
+                    </span>
+                    <button> Delete Finished Task</button>
+                </div>
             </div>
         )
     }
